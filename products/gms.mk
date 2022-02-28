225e1dcdcb8c2d76a9949ba7f6702a8492f5332c
Jakob <36961499+Jak0b-cli@users.noreply.github.com>
2022-02-28 18:29:37 +0100

    Update gms.mk

--- products/gms.mk
+++ products/gms.mk
@@ -1,9 +1,3 @@
 PRODUCT_PACKAGES += \
-	GmsCore \
-	GsfProxy \
-	FakeStore \
-	IchnaeaNlpBackend \
-	NominatimGeocoderBackend \
 	FDroid \
-	FDroidPrivilegedExtension \
-	additional_repos.xml
+	FDroidPrivilegedExtension 
